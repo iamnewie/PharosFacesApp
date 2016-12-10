b933df9eaf7ec4d6689040d30faa19e432af542b
iamnewie <iamnewie@gmail.com>
2016-12-10 20:06:37 +0700

    add layout files

--- app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/CameraFragment.java
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/CameraFragment.java
@@ -1,7 +1,5 @@
 package com.google.android.gms.samples.vision.face.facetracker;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,11 +10,12 @@ import android.view.ViewGroup;
 /**
  * Created by Home on 12/10/2016.
  */
-public class LoginFragment extends Fragment {
+public class CameraFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.loginlayout,container,false);
+        return inflater.inflate(R.layout.main,container,false);
+
     }
 }
